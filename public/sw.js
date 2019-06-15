451ae68ebac82e8ff78c55b078ce8f5c55e2b467
Kirill Konshin <kirill.konshin@gmail.com>
2019-06-14 19:23:40 -0700

    Code improvements

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,20 @@
 importScripts('../node_modules/@babel/standalone/babel.js');
 
-self.addEventListener('activate', event => {
-    //@see https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
-    clients.claim();
-});
+// this is needed to activate the worker immediately without reload
+//@see https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+self.addEventListener('activate', event => clients.claim());
 
 self.addEventListener('fetch', (event) => {
 
     const {request: {url}} = event;
 
-    if (url.includes('/react/') || url.includes('/react-dom/')) { //url.endsWith('.js') &&
+    console.log('Req', url);
+
+    if (url.includes('/react/') || url.includes('/react-dom/')) {
         event.respondWith(
             fetch(url)
-                .then((response) => response.text())
-                .then((body) => {
+                .then(response => response.text())
+                .then(body => {
 
                     body = body.replace(/(typeof exports === 'object')/ig, 'return $1');
                     body = body.replace('(function (global, factory) {', 'export default (function (global = window, factory) {');
@@ -28,33 +29,30 @@ self.addEventListener('fetch', (event) => {
     } else if (event.request.url.endsWith('.css')) {
         event.respondWith(
             fetch(event.request.url)
-                .then((response) => response.text())
-                .then((body) => {
-                    // Export the response body as a JavaSript string.
-                    // The response body has to be sanitized before turning it
-                    // into JavaScript code.
-                    // Credits: https://stackoverflow.com/a/22837870
-                    const newBody = `
+                .then(response => response.text())
+                .then(body => new Response(
+                    //TODO We don't track instances, so 2x import will result in 2x <style> tags
+                    `
                         const head = document.getElementsByTagName('head')[0];
                         const style = document.createElement('style');
                         style.setAttribute('type', 'text/css');
                         style.appendChild(document.createTextNode("${JSON.stringify(body).slice(1, -1)}"));
                         head.appendChild(style);
-                        export default null;`;
-
-                    // Replace the original response with an ES6 module
-                    return new Response(newBody, {
+                        export default null;
+                    `,
+                    {
                         headers: new Headers({
                             'Content-Type': 'application/javascript'
                         })
                     })
-                })
+                )
         )
     } else if (event.request.url.endsWith('.jsx')) {
         event.respondWith(
             fetch(event.request.url)
-                .then((response) => response.text())
-                .then((body) => new Response(
+                .then(response => response.text())
+                .then(body => new Response(
+                    //TODO Cache
                     Babel.transform(body, {
                         presets: [
                             'react',
@@ -64,7 +62,7 @@ self.addEventListener('fetch', (event) => {
                         ],
                         sourceMaps: true
                     }).code,
-                    { //TODO Cache
+                    {
                         headers: new Headers({
                             'Content-Type': 'application/javascript'
                         })
