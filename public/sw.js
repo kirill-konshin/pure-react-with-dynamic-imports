d6ff8460a38f28e8b815ee2734a2be5dd237c533
Kirill Konshin <kirill.konshin@gmail.com>
2019-06-17 16:46:59 -0700

    Code improvements

--- public/sw.js
+++ public/sw.js
@@ -4,27 +4,42 @@ importScripts('../node_modules/@babel/standalone/babel.js');
 //@see https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
 self.addEventListener('activate', event => clients.claim());
 
+const globalMap = {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+};
+
+const getGlobalByUrl = (url) => Object.keys(globalMap).reduce((res, key) => {
+    if (res) return res;
+    if (matchUrl(url, key)) return globalMap[key];
+    return res;
+}, null);
+
+const matchUrl = (url, key) => url.includes(`/${key}/`);
+
 self.addEventListener('fetch', (event) => {
 
     const {request: {url}} = event;
 
     console.log('Req', url);
 
-    if (url.includes('/react/') || url.includes('/react-dom/')) {
+    if (Object.keys(globalMap).some(key => matchUrl(url, key))) {
         event.respondWith(
             fetch(url)
                 .then(response => response.text())
-                .then(body => {
-
-                    body = body.replace(/(typeof exports === 'object')/ig, 'return $1');
-                    body = body.replace('(function (global, factory) {', 'export default (function (global = window, factory) {');
-
-                    return new Response(body, {
+                .then(body => new Response(`
+                        const head = document.getElementsByTagName('head')[0];
+                        const script = document.createElement('script');
+                        script.innerHTML = "${JSON.stringify(body).slice(1, -1)}";
+                        script.setAttribute('type', 'text/javascript');
+                        head.appendChild(script);
+                        export default window.${getGlobalByUrl(url)};
+                    `, {
                         headers: new Headers({
                             'Content-Type': 'application/javascript'
                         })
                     })
-                })
+                )
         )
     } else if (event.request.url.endsWith('.css')) {
         event.respondWith(
